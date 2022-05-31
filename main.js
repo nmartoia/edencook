053ec6d35ca9e81937dd7af72dd3a6c02a487b64
nmartoia <nmartoia@edenschool.fr>
2022-05-31 11:40:32 +0200

    le menue burger s'active

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 import './style.css'
+const recette = document.querySelectorAll(".recette");
+const ingredient = document.querySelectorAll(".ingredient");
+let count=0;
+const mburger =document.querySelector(".fa-bars")
+const close = document.querySelector('#close')
+const nav = document.querySelector('nav');
+mburger.addEventListener("click",()=>{
+nav.style.right='0'
+})
+close.addEventListener('click',()=>{
+  nav.style.right='-500px'
+})
+for (let i = 0; i < recette.length; i++) {
+  recette[i].addEventListener("click",()=>{
+    if(count===i&&ingredient[i].style.display=="block"){
+      ingredient[i].style.display="none"
+    }else{
+      ingredient[count].style.display="none"
+      ingredient[i].style.display="block"
+      count=i
+    }
+  })
+}
